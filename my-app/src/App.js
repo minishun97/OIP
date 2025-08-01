57b27f36e653a5be7127d033cb511e29de048f9b
Akikokoro <jefftanzhiwen@gmail.com>
2025-08-01 21:58:54 +0800

    Add deployment codes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { HashRouter as Router, Route, Routes} from 'react-router-dom';
 import './components/base.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
