8f1802e06a946672fa371fb1a9e2317ef65a9c2d
Akikokoro <jefftanzhiwen@gmail.com>
2025-08-05 17:26:10 +0800

    Updated navbar for responsive web design

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,54 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
 import './Navbar.css';
 
-const Navbar = () => (
-    <nav className="navbar">
-        <ul className="navbar_links">
-            <li>
-                <NavLink to="/" activeClassName="active">
-                    HOME
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/project-journey" activeClassName="active">
-                    PROJECT JOURNEY
-                </NavLink>
-            </li>
-            <li className="dropdown">
-                <NavLink to="/reflection/team-reflection" activeClassName="active" className="dropdown-toggle">
-                    REFLECTION
-                </NavLink>
-                <ul className="dropdown-menu">
+const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [reflectionOpen, setReflectionOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const toggleReflection = () => {
+        setReflectionOpen(!reflectionOpen);
+    };
+
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <h1 className="logo-text">
+                    <span>Friends of</span>
+                    <span>the River Kelvin</span>
+                </h1>
+            </div>
+
+            <div className={`navbar-links ${menuOpen ? 'open' : ''}`}>
+                <ul className="navbar_links">
                     <li>
-                        <NavLink to="/reflection/team-reflection" activeClassName="active">
-                            Team Reflection
+                        <NavLink to="/" activeClassName="active">
+                            HOME
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/reflection/team-contract" activeClassName="active">
-                            Team Contract
+                        <NavLink to="/project-journey" activeClassName="active">
+                            PROJECT JOURNEY
                         </NavLink>
                     </li>
-                    <li>
-                        <NavLink to="/reflection/questionnaires" activeClassName="active">
-                            Questionnaires
-                        </NavLink>
+                    <li 
+                        className={`dropdown ${reflectionOpen ? 'active' : ''}`} 
+                        onClick={toggleReflection}
+                        onMouseEnter={() => setReflectionOpen(true)}
+                        onMouseLeave={() => setReflectionOpen(false)}
+                    >
+                        <span className="dropdown-toggle">
+                            REFLECTION
+                        </span>
+                        {/* Sub-menu for Reflection */}
+                        <ul className={`dropdown-menu ${reflectionOpen ? 'open' : ''}`}>
+                            <li>
+                                <NavLink to="/reflection/team-reflection" activeClassName="active">
+                                    Team Reflection
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/reflection/team-contract" activeClassName="active">
+                                    Team Contract
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/reflection/questionnaires" activeClassName="active">
+                                    Questionnaires
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/reflection/prototyping" activeClassName="active">
+                                    Prototyping
+                                </NavLink>
+                            </li>
+                        </ul>
                     </li>
                     <li>
-                        <NavLink to="/reflection/prototyping" activeClassName="active">
-                            Prototyping
+                        <NavLink to="/about-us" activeClassName="active">
+                            ABOUT US
                         </NavLink>
                     </li>
                 </ul>
-            </li>
-            <li>
-                <NavLink to="/about-us" activeClassName="active">
-                    ABOUT US
-                </NavLink>
-            </li>
-        </ul>
-    </nav>
-);
+            </div>
+
+            <div className="hamburger" onClick={toggleMenu}>
+                <FaBars />
+            </div>
+        </nav>
+    );
+};
 
 export default Navbar;
