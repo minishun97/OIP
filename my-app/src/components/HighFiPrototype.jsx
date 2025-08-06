0b6064cdc2de0b1d1e5c01d7d7da51aaad558a81
Akikokoro <jefftanzhiwen@gmail.com>
2025-08-06 16:54:26 +0800

    Fix bugs

--- my-app/src/components/HighFiPrototype.jsx
+++ my-app/src/components/HighFiPrototype.jsx
@@ -24,11 +24,12 @@ const HighFiPrototype = () => (
           style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
           width="400"
           height="250"
-          src="https://embed.figma.com/design/t62H9YivWSf22oHUXzveJs/OIP-FORK-PROTOTYPE?node-id=0-1&embed-host=share"
+          src="https://embed.figma.com/proto/t62H9YivWSf22oHUXzveJs/OIP-FORK-PROTOTYPE?node-id=73-327&p=f&scaling=min-zoom&content-scaling=fixed&page-id=19%3A114&starting-point-node-id=73%3A345&show-proto-sidebar=1&embed-host=share"
           allowFullScreen
           title="Figma Embed Mobile"
         />
       </div>
+
 
       {/* Website Prototype */}
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
@@ -37,7 +38,7 @@ const HighFiPrototype = () => (
           style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
           width="400"
           height="250"
-          src="https://embed.figma.com/design/t62H9YivWSf22oHUXzveJs/OIP-FORK-PROTOTYPE?node-id=0-1&embed-host=share"
+          src="https://embed.figma.com/proto/t62H9YivWSf22oHUXzveJs/OIP-FORK-PROTOTYPE?node-id=82-96&scaling=min-zoom&content-scaling=fixed&page-id=19%3A228&starting-point-node-id=82%3A96&show-proto-sidebar=1&embed-host=share"
           allowFullScreen
           title="Figma Embed Website"
         />
