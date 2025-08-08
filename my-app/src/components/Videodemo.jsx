53d5f185bc43f86c00bcedebd16885341a46e84a
minishun97 <jieshun97@gmail.com>
2025-08-08 14:32:30 +0100

    Update Videodemo.jsx

--- my-app/src/components/Videodemo.jsx
+++ my-app/src/components/Videodemo.jsx
@@ -18,7 +18,7 @@ const Videodemo = () => {
       <iframe
         width="560"
         height="315"
-        src="https://youtu.be/hBzCDKjtbQo"
+        src="https://www.youtube.com/embed/hBzCDKjtbQo"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
